231c0299db7e3925615ec30619b9134cda7f5407
Giacomo Ronconi <36189778+giacomoronconiobda@users.noreply.github.com>
2022-04-05 12:59:29 +0200

    Create run.sh

---
+++ base/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+############################################################
+# Sparqling Startup Script
+############################################################
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Set HOME location
+HOME="${SCRIPT_DIR}"
+JAR="sparqling-ws-1.0.0.jar"
+
+# Check that java can be found in PATH
+if [[ -n "$JAVA_HOME" ]]; then
+    JAVA="$JAVA_HOME/bin/java"
+else
+    JAVA="$(which java)"
+fi
+
+if [[ -z "$JAVA" ]] || [[ ! -x "$JAVA" ]]; then
+    echo "Unable to find java executable, exiting..."
+    exit 1
+fi
+
+echo "Using java from: $JAVA"
+
+# Java options
+JAVA_OPTS="${JAVA_OPTS:-Dlogback.configurationFile="$HOME/logback.xml"}"
+# Java extra (platform-specific) options 
+JAVA_EXT_OPTS=
+
+# Start Jetty WebServer
+"$JAVA" \
+    $JAVA_OPTS ${JAVA_EXT_OPTS:+"$JAVA_EXT_OPTS"} \
+    "$HOME/$JAR" \
+    "$HOME/ui"
