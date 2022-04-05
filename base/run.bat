73a4a2a0b23a198e480109cd9a7a500163f40545
Giacomo Ronconi <36189778+giacomoronconiobda@users.noreply.github.com>
2022-04-05 12:54:52 +0200

    Create run.bat

---
+++ base/run.bat
@@ -0,0 +1,15 @@
+:::::::::::::::::::::::::::::::::::
+:: Sparqling Startup Script (WIN)
+:::::::::::::::::::::::::::::::::::
+
+:: Set SPARQLING location
+SET HOME=%~dp0
+SET JAR=sparqling-ws-1.0.0.jar
+
+:: Set java options: log cofnigurations and file encoding
+SET JAVA_OPTS=-Dlogback.configurationFile=%MONOLITH_HOME%logback.xml -Dfile.encoding=UTF-8
+
+:: Launch MWS (Powered by Jetty) and Monolith
+java %JAVA_OPTS% ^
+	-cp "%HOME%%JAR%" ^
+	"%HOME%ui"
