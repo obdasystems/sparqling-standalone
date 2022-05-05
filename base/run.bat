eb0d48b6366c955b1c6d4b3414eb5b933ae4d813
Giacomo Ronconi <36189778+giacomoronconiobda@users.noreply.github.com>
2022-05-05 11:02:02 +0200

    Update run.bat

--- base/run.bat
+++ base/run.bat
@@ -10,6 +10,6 @@ SET JAR=sparqling-ws-1.0.0.jar
 SET JAVA_OPTS=-Dlogback.configurationFile=%MONOLITH_HOME%logback.xml -Dfile.encoding=UTF-8
 
 :: Launch MWS (Powered by Jetty) and Monolith
-java %JAVA_OPTS% ^
+START "" javaw %JAVA_OPTS% ^
 	-jar "%HOME%%JAR%" ^
 	"%HOME%ui"
